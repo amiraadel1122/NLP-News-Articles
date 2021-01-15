38bf6098a578ef4925d2571457f44a1b17d09eb0
amiraadel1122 <amiraadel1122@gmail.com>
2021-01-15 08:43:41 +0200

    api data

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,31 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let userUrl = document.getElementById('userUrl').value
+    Client.checkForName(userUrl)
+if(!userUrl){
+    alert('enter a url')
+}
+    console.log("::: Form Submitted :::")
+     fetch('http://localhost:8080/Apidata', {
+         method: "POST",
+         credentials: "same-origin",
+         headers: {
+             "Content-Type": "application/json",
+         },
+           body: JSON.stringify({
+            text: userUrl
+           })
+     })
+     .then(res => res.json())
+     .then(function(res) {
+         console.log('here from client')
+        document.getElementById('results').innerHTML = res.message
+     })
+    
+    
+
+}
+
+export { handleSubmit }
---
+++ src/client/js/index.js
@@ -0,0 +1,13 @@
+import '../styles/base.scss'
+import '../styles/footer.scss'
+import '../styles/form.scss'
+import '../styles/header.scss'
+
+import { handleSubmit } from './formHandler'
+import { checkForName } from './nameChecker'
+
+
+export {
+     checkForName,
+     handleSubmit
+    }
